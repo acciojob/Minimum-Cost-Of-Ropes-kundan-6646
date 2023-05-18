7382e41ce5e3be7bf15554eff8a4c26024f44e6b
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-05-18 22:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const input = document.getElementById('rope-lengths');
+const result = document.getElementById('result');
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+	let sum = 0;
+	let arr = [];
+	let inputStr = input.value.split(',');
+	for (let num of inputStr) {
+		arr.push(parseInt(num));
+	}
+
+	while (arr.length > 1) {
+		arr.sort(function(a, b){return a - b});
+		// console.log(arr);
+		let mergedRope = arr[0] + arr[1];
+		sum += mergedRope;
+		arr.splice(0, 2);
+		arr.push(mergedRope);
+	}
+	result.innerText = sum;
 }  
